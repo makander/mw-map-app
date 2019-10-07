914ff0bd3c2f8606ebb86901bda7f9f20df79470
Nils Makander <makander@gmail.com>
2019-10-07 17:11:18 +0200

    follow docs for React MapboxGL and add state + event handlers for getting coords

--- src/components/map.js
+++ src/components/map.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MapGL from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
-const token = process.env.REACT_TOKEN;
+
+const token = process.env.REACT_APP_TOKEN;
 
 const Map = () => {
-  return <MapGL mapboxApiAccessToken={token}></MapGL>;
+  const [viewport, setViewport] = useState({
+    width: 400,
+    height: 400,
+    latitude: 37.7577,
+    longitude: 122.4376,
+    zoom: 8,
+  });
+
+  return (
+    <MapGL
+      {...viewport}
+      mapboxApiAccessToken={token}
+      onViewportChange={(viewport) => setViewport({ ...viewport })}
+      onClick={(e) => {
+        console.log(e, viewport);
+      }}></MapGL>
+  );
 };
 
 export default Map;
