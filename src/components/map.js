b860dad5f2a1e46aea8ec1364008078fa79ca9b4
Nils <nils@mw-008.local>
2019-10-08 16:22:51 +0200

    build state and queries for wikipedia and weather, will continue to build out views with cards

--- src/components/map.js
+++ src/components/map.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import MapGL from 'react-map-gl';
-import 'mapbox-gl/dist/mapbox-gl.css';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import MapGL from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import axios from "axios";
 
 const token = process.env.REACT_APP_TOKEN;
 const owToken = process.env.REACT_APP_OW_TOKEN;
-const cors = process.env.REACT_APP_CORS;
+//const cors = process.env.REACT_APP_CORS;
 
 const Map = () => {
   const [viewport, setViewport] = useState({
@@ -13,36 +13,75 @@ const Map = () => {
     height: 400,
     latitude: 59.3293,
     longitude: 18.0686,
-    zoom: 8,
+    zoom: 8
   });
 
-  const handleClick = (e) => {
+  const [location, setLocation] = useState({
+    country: null,
+    name: null
+  });
+
+  const [wikidata, setWikidata] = useState({
+    extract: {},
+    displayTitle: {},
+    image: null
+  });
+
+  const [dailyWeather, setDailyWeather] = useState({});
+  useEffect(() => {
+    if (location.country !== null) {
+      axios
+        .all([
+          axios.get(
+            `https://en.wikipedia.org/api/rest_v1/page/summary/${location.name}`
+          ),
+          axios.get(
+            `https://en.wikipedia.org/api/rest_v1/page/summary/${location.country}`
+          )
+        ])
+        .then(
+          axios.spread((name, country) => {
+            setWikidata({
+              extract: name.data.extract,
+              displayTitle: name.data.displaytitle,
+              image: country.data.thumbnail.source
+            });
+          })
+        );
+    }
+  }, [location]);
+
+  const handleClick = async e => {
     e.preventDefault();
-    axios
-      .get(
-        `https://api.openweathermap.org/data/2.5/forecast?lat=${viewport.latitude}&lon=${viewport.longitude}&appid=${owToken}`
-      )
-      .then((data) => console.log(data));
-    axios
-      .get(
-        `${cors}/https://en.wikipedia.org/w/api.php?action=query&list=geosearch&format=json&gscoord=${viewport.latitude}|${viewport.longitude}&gsradius=10000&gslimit=10`
-      )
-      //
-      .then((res) => console.log('wiki', res));
+    console.log(e);
 
     axios
-      .get(
-        `${cors}/https://en.wikipedia.org/w/api.php?action=query&prop=images&imlimit=max&generator=geosearch&ggsradius=10000&ggscoord=${viewport.latitude}|${viewport.longitude}&format=json`
-      )
-      .then((res) => console.log('wiki images', res));
+      .all([
+        axios.get(
+          `https://api.openweathermap.org/data/2.5/forecast?lat=${viewport.latitude}&lon=${viewport.longitude}&appid=${owToken}`
+        ),
+        axios.get(
+          `https://api.mapbox.com/geocoding/v5/mapbox.places/${viewport.longitude},${viewport.latitude}.json?access_token=${token}&types=country,place`
+        )
+      ])
+      .then(
+        axios.spread((weather, res) => {
+          console.log("weather", weather);
+          setDailyWeather({ weather });
+          setLocation({
+            name: res.data.features[0].text,
+            country: res.data.features[1].text
+          });
+        })
+      );
   };
 
   return (
     <MapGL
       {...viewport}
       mapboxApiAccessToken={token}
-      onViewportChange={(viewport) => setViewport({ ...viewport })}
-      onClick={(e) => {
+      onViewportChange={viewport => setViewport({ ...viewport })}
+      onClick={e => {
         handleClick(e);
       }}></MapGL>
   );
